a47708cc193eb940f2a168a72e4805048d1a3671
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-05-31 11:58:38 +0300

    remove whitelist for cors

--- src/services/cors.service.js
+++ src/services/cors.service.js
@@ -8,43 +8,13 @@ const enable =
   config.has('app.cors.enable') &&
   config.get('app.cors.enable')
 
-const _getWhitelist = (confPath = 'app.cors.whitelist') => {
-  if (Array.isArray(config.get(confPath))) {
-    return config.get(confPath)
-  }
-
-  if (typeof config.get(confPath) === 'string') {
-    return [config.get(confPath)]
-  }
-
-  return null
-}
-
 const corsBase = () => {
   if (!enable) {
     return (req, res, next) => next()
   }
 
-  let whitelist = _getWhitelist()
-
-  const originFn = (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-
-      return
-    }
-
-    callback(new Error('Not allowed by CORS'))
-  }
-
-  let origin = true
-
-  if (whitelist !== null) {
-    origin = originFn
-  }
-
   const corsOptions = {
-    origin,
+    origin: true,
     methods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
     credentials: true,
