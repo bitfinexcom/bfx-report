95438015171d65730f666891fd30e488a7c2300e
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-06-05 17:16:33 +0300

    remove reference to api v1

--- workers/loc.api/bitfinex-api-node/index.js
+++ workers/loc.api/bitfinex-api-node/index.js
@@ -1,28 +1,22 @@
 'use strict'
 
 const BFXBase = require('bitfinex-api-node')
-const { RESTv1 } = require('bitfinex-api-node')
 const RESTv2 = require('./lib/transports/rest2')
 
 class BFX extends BFXBase {
   /**
    * @param {number} versio
    * @param {Object} extraOpts
-   * @return {RESTv1|RESTv2}
+   * @return {RESTv2}
    */
-  rest (version = 2, extraOpts = {}) {
-    if (version !== 1 && version !== 2) {
-      throw new Error(`invalid http API version: ${version}`)
-    }
-
-    const key = `${version}|${JSON.stringify(extraOpts)}`
+  rest (version, extraOpts = {}) {
+    const key = `2|${JSON.stringify(extraOpts)}`
 
     if (!this._transportCache.rest[key]) {
       Object.assign(extraOpts, this._restArgs)
       const payload = this._getTransportPayload(extraOpts)
 
-      this._transportCache.rest[key] =
-        version === 2 ? new RESTv2(payload) : new RESTv1(payload)
+      this._transportCache.rest[key] = new RESTv2(payload)
     }
 
     return this._transportCache.rest[key]
