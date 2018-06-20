51fb476e2fba1aae3c1b8773eaca713f64e32e73
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-06-20 18:21:25 +0300

    add bull queue, create method to get full data

--- workers/api.service.report.wrk.js
+++ workers/api.service.report.wrk.js
@@ -18,8 +18,82 @@ class WrkReportServiceApi extends WrkApi {
     }
   }
 
+  getPluginCtx (type) {
+    super.init()
+
+    const ctx = super.getPluginCtx(type)
+
+    switch (type) {
+      case 'api_bfx':
+        ctx.bull_processor = this.bull_processor
+        ctx.bull_aggregator = this.bull_aggregator
+        break
+    }
+
+    return ctx
+  }
+
   init () {
     super.init()
+
+    this.setInitFacs([
+      [
+        'fac',
+        'bfx-facs-bull',
+        'processor',
+        'processor',
+        () => this.getBullProcConf()
+      ],
+      [
+        'fac',
+        'bfx-facs-bull',
+        'aggregator',
+        'aggregator',
+        () => this.getBullAggrConf()
+      ]
+    ])
+  }
+
+  getBullProcConf () {
+    const group = this.group
+    const conf = this.conf[group]
+
+    if (
+      conf &&
+      typeof conf.bull === 'object' &&
+      conf.bull.processor &&
+      conf.bull.processor.queue
+    ) {
+      return {
+        port: conf.bull.port,
+        host: conf.bull.host,
+        auth: conf.bull.auth,
+        queue: conf.bull.processor.queue
+      }
+    }
+
+    return null
+  }
+
+  getBullAggrConf () {
+    const group = this.group
+    const conf = this.conf[group]
+
+    if (
+      conf &&
+      typeof conf.bull === 'object' &&
+      conf.bull.aggregator &&
+      conf.bull.aggregator.queue
+    ) {
+      return {
+        port: conf.bull.port,
+        host: conf.bull.host,
+        auth: conf.bull.auth,
+        queue: conf.bull.aggregator.queue
+      }
+    }
+
+    return null
   }
 }
 
--- workers/loc.api/service.report.js
+++ workers/loc.api/service.report.js
@@ -2,6 +2,7 @@
 
 const { Api } = require('bfx-wrk-api')
 const { getREST, getLimitNotMoreThan } = require('./helpers')
+const { promisify } = require('util')
 
 class ReportService extends Api {
   space (service, msg) {
@@ -118,6 +119,57 @@ class ReportService extends Api {
     } catch (err) {
       cb(err)
     }
+  }
+
+  // TODO: 
+  async getTradesCsv (space, args, cb) {
+    try {
+      if (!args.params || typeof args.params !== 'object') {
+        throw new Error('ERR_ARGS_NO_PARAMS')
+      }
+
+      args.params.limit = 1000
+      args.params.start = null
+      args.params.end = null
+
+      const result = await this._getFullData('getTrades', args)
+
+      cb(null, result)
+    } catch (err) {
+      cb(err)
+    }
+  }
+
+  // TODO: 
+  async _getFullData (method, args, propName = 'mts') {
+    if (typeof this[method] !== 'function') {
+      throw new Error('ERR_METHOD_NOT_FOUND')
+    }
+
+    const data = []
+    const getData = promisify(this[method])
+    const res = await getData(null, args)
+
+    data.push(...res)
+
+    if (
+      data &&
+      Array.isArray(data) &&
+      data.length > 0 &&
+      typeof data[data.length - 1] === 'object' &&
+      data[data.length - 1][propName] &&
+      Number.isInteger(data[data.length - 1][propName])
+    ) {
+      console.log('---Data length---', data.length)
+      args.params.end = data[data.length - 1][propName] - 1
+
+      const subRes = await this._getFullData(method, args, propName)
+
+      data.push(...subRes)
+    }
+
+    console.log('---Total data length---', data.length)
+    return data
   }
 }
 
