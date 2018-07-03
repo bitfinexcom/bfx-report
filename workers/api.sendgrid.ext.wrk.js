aaac1f1ac159af7515d31de823e1bda08891e5a8
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-07-03 16:57:39 +0300

    add single launch point for all workers for dev env

---
+++ workers/api.sendgrid.ext.wrk.js
@@ -0,0 +1,3 @@
+'use strict'
+
+module.exports = require('bfx-ext-js/workers/api.sendgrid.ext.wrk')
---
+++ workers/loc.api/sendgrid.ext.js
@@ -0,0 +1,3 @@
+'use strict'
+
+module.exports = require('bfx-ext-js/workers/loc.api/sendgrid.ext')
