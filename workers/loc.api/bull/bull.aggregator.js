6242a2f312b6d0f0b783125342bc3c40e8e3f66b
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-07-13 15:43:43 +0300

    remove console.log

--- workers/loc.api/bull/bull.aggregator.js
+++ workers/loc.api/bull/bull.aggregator.js
@@ -1,6 +1,5 @@
 'use strict'
 
 module.exports = async (job) => {
-  console.log('---started aggregator---') // TODO: need to remove
   return Promise.resolve()
 }
--- workers/loc.api/bull/bull.processor.js
+++ workers/loc.api/bull/bull.processor.js
@@ -1,6 +1,5 @@
 'use strict'
 
 module.exports = async (job) => {
-  console.log('---started processor---') // TODO: need to remove
   return Promise.resolve()
 }
