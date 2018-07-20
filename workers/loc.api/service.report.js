e834c01ce80f8f6ed215495b4e42b73a748cbf0a
ezeswci <wernicke.ezequiel@gmail.com>
2018-07-20 16:50:21 -0300

    fixes and re factor

--- workers/loc.api/service.report.js
+++ workers/loc.api/service.report.js
@@ -3,7 +3,7 @@
 const { Api } = require('bfx-wrk-api')
 const {
   getREST,
-  getLimitNotMoreThan
+  getParams
 } = require('./helpers')
 
 class ReportService extends Api {
@@ -25,101 +25,58 @@ class ReportService extends Api {
 
   async getLedgers (space, args, cb) {
     try {
-      const params = []
-
-      if (args.params) {
-        if (typeof args.params !== 'object') {
-          throw new Error('ERR_ARGS_NO_PARAMS')
-        }
-
-        params.push(
-          ...[
-            args.params.symbol,
-            args.params.start,
-            args.params.end,
-            getLimitNotMoreThan(args.params.limit)
-          ]
-        )
-      }
-
+      const maxLimit = 5000
+      const params = getParams(args, maxLimit)
       const rest = getREST(args.auth, this.ctx.grc_bfx.caller)
       const result = await rest.ledgers(...params)
 
       cb(null, result)
     } catch (err) {
+      console.log('getLedgers error: ', err.toString())
       cb(err)
     }
   }
 
   async getTrades (space, args, cb) {
     try {
-      if (!args.params || typeof args.params !== 'object') {
-        throw new Error('ERR_ARGS_NO_PARAMS')
-      }
-
-      const params = [
-        args.params.symbol,
-        args.params.start,
-        args.params.end,
-        getLimitNotMoreThan(args.params.limit),
-        args.params.sort
-      ]
-
+      const maxLimit = 1500
+      const params = getParams(args, maxLimit)
       const rest = getREST(args.auth, this.ctx.grc_bfx.caller)
       const result = await rest.accountTrades(...params)
 
       cb(null, result)
     } catch (err) {
+      console.log('getMovements error: ', err.toString())
       cb(err)
     }
   }
 
   async getOrders (space, args, cb) {
     try {
-      if (!args.params || typeof args.params !== 'object') {
-        throw new Error('ERR_ARGS_NO_PARAMS')
-      }
-
-      const params = [
-        args.params.symbol,
-        args.params.start,
-        args.params.end,
-        getLimitNotMoreThan(args.params.limit)
-      ]
-
+      const maxLimit = 5000
+      const params = getParams(args, maxLimit)
       const rest = getREST(args.auth, this.ctx.grc_bfx.caller)
       const result = await rest.orderHistory(...params)
 
       cb(null, result)
     } catch (err) {
+      console.log('getOrders error: ', err.toString())
       cb(err)
     }
   }
 
   async getMovements (space, args, cb) {
     try {
-      const params = []
-
-      if (args.params) {
-        if (typeof args.params !== 'object') {
-          throw new Error('ERR_ARGS_NO_PARAMS')
-        }
-
-        params.push(
-          ...[
-            args.params.symbol,
-            args.params.start,
-            args.params.end,
-            getLimitNotMoreThan(args.params.limit)
-          ]
-        )
-      }
-
+      const maxLimit = 25
+      console.log('args', args)
+      const params = getParams(args, maxLimit)
+      console.log('params', params) // simbol start end limit
       const rest = getREST(args.auth, this.ctx.grc_bfx.caller)
       const result = await rest.movements(...params)
-
+      console.log('result', result)
       cb(null, result)
     } catch (err) {
+      console.log('getMovements error: ', err.toString())
       cb(err)
     }
   }
