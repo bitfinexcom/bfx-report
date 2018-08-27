2773ec28db939207242be33296a1460962e4daa2
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-08-27 17:53:22 +0300

    add methods for querying db

--- workers/loc.api/service.report.mongo.js
+++ workers/loc.api/service.report.mongo.js
@@ -1,6 +1,8 @@
 'use strict'
 
 const MediatorReportService = require('./service.report.mediator')
+const { getLimitNotMoreThan } = require('./helpers')
+const { getMethodCollMap } = require('./sync/helpers')
 
 class MongoReportService extends MediatorReportService {
   // TODO:
@@ -21,7 +23,43 @@ class MongoReportService extends MediatorReportService {
   async getLedgers (space, args, cb) {
     if (super.getLedgers(space, args, cb)) return
 
-    cb(new Error('NOT_IMPLEMENTED'))
+    if (
+      !args.params ||
+      typeof args.params !== 'object'
+    ) {
+      throw new Error('ERR_ARGS_NO_PARAMS')
+    }
+
+    try {
+      const maxLimit = 5000
+      const params = { ...args.params }
+      params.limit = getLimitNotMoreThan(args.params.limit, maxLimit)
+      const method = '_getLedgers'
+      const methodColl = getMethodCollMap().get(method)
+      const query = {}
+
+      query[methodColl.dateFieldName] = {
+        $gte: params.start ? params.start : 0,
+        $lte: params.end ? params.end : (new Date()).getTime()
+      }
+
+      if (params.symbol) {
+        query[methodColl.symbolFieldName] = params.symbol
+      }
+
+      const res = await this._getCollByName(methodColl.name)
+        .find(
+          query,
+          {
+            sort: [[methodColl.dateFieldName, -1]],
+            limit: params.limit
+          })
+        .toArray()
+
+      cb(null, res)
+    } catch (err) {
+      cb(err)
+    }
   }
 
   // TODO:
@@ -45,19 +83,25 @@ class MongoReportService extends MediatorReportService {
     cb(new Error('NOT_IMPLEMENTED'))
   }
 
-  async _createIndex () {
+  _getCollByName (name) {
     const db = this.ctx.dbMongo_m0.db
 
-    await db.collection('trades').createIndex({ 'mtsCreate': -1, 'pair': 1 })
-    await db.collection('ledgers').createIndex({ 'mts': -1, 'currency': 1 })
-    await db.collection('orders').createIndex({ 'mtsUpdate': -1, 'symbol': 1 })
-    await db.collection('movements').createIndex({ 'mtsUpdated': -1, 'currency': 1 })
+    return db.collection(name)
+  }
+
+  async _createIndex () {
+    await this._getCollByName('trades').createIndex({ 'mtsCreate': -1, 'pair': 1 })
+    await this._getCollByName('ledgers').createIndex({ 'mts': -1, 'currency': 1 })
+    await this._getCollByName('orders').createIndex({ 'mtsUpdate': -1, 'symbol': 1 })
+    await this._getCollByName('movements').createIndex({ 'mtsUpdated': -1, 'currency': 1 })
   }
 
   async _getLastElemFromDb (name, dateFieldName) {
-    const db = this.ctx.dbMongo_m0.db
+    return this._getCollByName(name).findOne({ }, { sort: [[dateFieldName, -1]] })
+  }
 
-    return db.collection(name).findOne({ }, { sort: [[dateFieldName, -1]] })
+  async _insertElemsToDb (name, data = []) {
+    return this._getCollByName(name).insertMany(data, { ordered: true })
   }
 }
 
