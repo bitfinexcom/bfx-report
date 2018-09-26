50972facb49bd5f001d43ab8348b0d645e3b175a
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-09-26 17:07:22 +0300

    fix db init

--- workers/loc.api/sync/dao/dao.sqlite.js
+++ workers/loc.api/sync/dao/dao.sqlite.js
@@ -59,8 +59,8 @@ class SqliteDAO extends DAO {
       await this._run('BEGIN TRANSACTION')
       await this._createTablesIfNotExists()
       await this._createIndexisIfNotExists()
-      await this.updateProgress('SYNCHRONIZATION_HAS_NOT_STARTED_YET')
       await this._run('COMMIT')
+      await this.updateProgress('SYNCHRONIZATION_HAS_NOT_STARTED_YET')
     } catch (err) {
       await this._run('ROLLBACK')
 
