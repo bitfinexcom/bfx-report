5adf9d590e8be6517191466879077bac43f361eb
ZIMkaRU <vsvoronkov@gmail.com>
2018-10-03 16:20:59 +0300

    change sync mode initialization

--- workers/api.service.report.wrk.js
+++ workers/api.service.report.wrk.js
@@ -163,6 +163,8 @@ class WrkReportServiceApi extends WrkApi {
           if (reportService._databaseInitialize) {
             try {
               await reportService._databaseInitialize()
+              await reportService.dao.updateStateOf('syncMode', true)
+              await reportService.dao.updateStateOf('scheduler', true)
             } catch (err) {
               this.logger.error(err.stack || err)
             }
