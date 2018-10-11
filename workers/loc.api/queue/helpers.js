73d98783f71adc6a9e6b21e6b3113ed5f828c723
Vladimir Voronkov <VSVoronkov@gmail.com>
2018-10-11 11:56:08 +0300

    add comment to archive

--- workers/loc.api/queue/helpers.js
+++ workers/loc.api/queue/helpers.js
@@ -442,6 +442,7 @@ const uploadS3 = async (
     zipFilePath = await createUniqueFileName(true)
     const writable = fs.createWriteStream(zipFilePath)
     const archive = archiver('zip', {
+      comment: fileNameWithoutExt,
       zlib: { level: 9 }
     })
 
