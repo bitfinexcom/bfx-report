8fab5e2682835876ea67e0123cdfbf811154260b
ZIMkaRU <vsvoronkov@gmail.com>
2019-01-04 17:58:13 +0200

    change sync initialization

--- workers/loc.api/sync/index.js
+++ workers/loc.api/sync/index.js
@@ -1,16 +1,77 @@
 'use strict'
 
+const syncQueue = require('./sync.queue')
+const {
+  setProgress,
+  getProgress,
+  logErrorAndSetProgress,
+  redirectRequestsToApi
+} = require('./helpers')
+const ALLOWED_COLLS = require('./allowed.colls')
+
 let reportService = null
 
-const sync = require('./sync')
+const _sync = async (isSkipSync, syncColls) => {
+  if (!isSkipSync) {
+    try {
+      await syncQueue.process()
+    } catch (err) {
+      await logErrorAndSetProgress(reportService, err)
+    }
+  }
 
-// TODO:
-module.exports = async (isSolveAfterRedirToApi, syncColls) => {
-  return sync(isSolveAfterRedirToApi, syncColls)
+  try {
+    await redirectRequestsToApi(reportService, false)
+  } catch (err) {
+    await logErrorAndSetProgress(reportService, err)
+  }
+
+  return getProgress(reportService)
+}
+
+module.exports = async (
+  isSolveAfterRedirToApi,
+  syncColls = ALLOWED_COLLS.ALL
+) => {
+  let isSkipSync = false
+
+  try {
+    const isEnable = await reportService.isSchedulerEnabled()
+    const currProgress = await getProgress(reportService)
+
+    if (isEnable) {
+      const mess = await syncQueue.add(syncColls)
+
+      if (mess) return mess
+    }
+    if (
+      (currProgress < 100) ||
+      !isEnable
+    ) {
+      return getProgress(reportService)
+    }
+
+    await reportService.pingApi()
+
+    await setProgress(reportService, 0)
+    await redirectRequestsToApi(reportService, true)
+  } catch (err) {
+    isSkipSync = true
+
+    await logErrorAndSetProgress(reportService, err)
+  }
+
+  if (!isSkipSync && isSolveAfterRedirToApi) {
+    _sync(isSkipSync).then(() => {}, () => {})
+
+    return 'SYNCHRONIZATION_IS_STARTED'
+  }
+
+  return _sync(isSkipSync, syncColls)
 }
 
 module.exports.setReportService = (rService) => {
   reportService = rService
 
-  sync.setReportService(rService)
+  syncQueue.injectDeps(rService)
 }
