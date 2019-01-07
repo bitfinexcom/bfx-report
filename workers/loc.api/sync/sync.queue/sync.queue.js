871a104af98729d3f9ef6526e16eefb86453e6c2
ZIMkaRU <vsvoronkov@gmail.com>
2019-01-07 16:00:38 +0200

    implement process method for SyncQueue

--- workers/loc.api/sync/sync.queue/sync.queue.js
+++ workers/loc.api/sync/sync.queue/sync.queue.js
@@ -10,6 +10,10 @@ const {
 } = require('../helpers')
 const ALLOWED_COLLS = require('../allowed.colls')
 
+const LOCKED_JOB_STATE = 'LOCKED'
+const NEW_JOB_STATE = 'NEW'
+const FINISHED_JOB_STATE = 'FINISHED'
+
 class SyncQueue extends EventEmitter {
   constructor (name) {
     super()
@@ -26,6 +30,7 @@ class SyncQueue extends EventEmitter {
     this.dao = dao
   }
 
+  // TODO: need mix `NEW` state to each obj of `data` arr
   async add (syncColls) {
     const _syncColls = Array.isArray(syncColls)
       ? syncColls
@@ -52,7 +57,45 @@ class SyncQueue extends EventEmitter {
     )
   }
 
-  async process () {}
+  async process () {
+    let count = 0
+
+    while (true) {
+      count += 1
+
+      const nextSync = await this._getNext()
+
+      if (
+        !nextSync ||
+        typeof nextSync !== 'object' ||
+        count > 100
+      ) {
+        break
+      }
+
+      await this._updateById(
+        nextSync._id,
+        { state: LOCKED_JOB_STATE }
+      )
+
+      const dataInserter = new DataInserter(
+        this.reportService,
+        nextSync.collName
+      )
+      dataInserter.setAsyncProgressHandler(progress => {
+        return this._asyncProgressHandler(count, progress)
+      })
+
+      await dataInserter.insertNewDataToDbMultiUser()
+      await this._updateById(
+        nextSync._id,
+        { state: FINISHED_JOB_STATE }
+      )
+    }
+
+    await this._removeByState(FINISHED_JOB_STATE)
+    await this.setProgress(100)
+  }
 
   _getAll (filter) {
     return this.dao.getElemsInCollBy(
@@ -77,9 +120,51 @@ class SyncQueue extends EventEmitter {
     }, [])
   }
 
-  async _getNext () {}
+  _getNext () {
+    return this.dao.getElemInCollBy(
+      this.name,
+      { state: [NEW_JOB_STATE, LOCKED_JOB_STATE] },
+      this._sort
+    )
+  }
 
-  async _remove (id) {}
+  _removeByState (state) {
+    return this.dao.removeElemsFromDb(
+      this.name,
+      null,
+      { state }
+    )
+  }
+
+  async _updateById (id, data) {
+    const res = await this.dao.updateCollBy(
+      this.name,
+      { _id: id },
+      data
+    )
+
+    if (res && res.changes < 1) {
+      throw new Error(`ERR_CAN_NOT_UPDATE_${this.name.toUpperCase()}_QUEUE_JOB_BY_ID_${id}`)
+    }
+  }
+
+  _getCount () {
+    return this.dao.getCountBy(this.name)
+  }
+
+  async _asyncProgressHandler (count, progress) {
+    const syncsAmount = await this._getCount()
+
+    if (count === 0 || progress === 0) {
+      return
+    }
+
+    const currProgress = Math.round(
+      ((count - 1 + (progress / 100)) / syncsAmount) * 100
+    )
+
+    await this.setProgress(currProgress)
+  }
 
   async setProgress (progress) {
     await setProgress(this.reportService, progress)
