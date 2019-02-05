ff404ada54c3de18893f14f240aa915274d22ab6
Vladimir Voronkov <VSVoronkov@gmail.com>
2019-02-05 14:49:57 +0200

    move express server to bfx-report-express

--- test/4-queue-load.spec.js
+++ test/4-queue-load.spec.js
@@ -20,7 +20,8 @@ const {
   testProcQueue
 } = require('./helpers/helpers.tests')
 
-const { app } = require('../app')
+process.env.NODE_CONFIG_DIR = path.join(__dirname, 'config')
+const { app } = require('bfx-report-express')
 const agent = request.agent(app)
 
 let wrksReportServiceApi = []
