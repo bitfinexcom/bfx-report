4cacf8f4f0cae9b29f2982e6938f315502154409
Vladimir Voronkov <VSVoronkov@gmail.com>
2019-07-10 16:17:40 +0300

    add sync/async handling to responder

--- workers/loc.api/responder/index.js
+++ workers/loc.api/responder/index.js
@@ -1,24 +1,53 @@
 'use strict'
 
-module.exports = async (logger, handler, name, cb) => {
+const _prepareErrorData = (err, name) => {
+  const _name = name
+    ? `\n  - METHOD_NAME: ${name}`
+    : ''
+  const _statusCode = err.statusCode
+    ? `\n  - STATUS_CODE: ${err.statusCode}`
+    : ''
+  const _options = err.options
+    ? `\n  - OPTION: ${err.options}`
+    : ''
+  const _err = `\n  - ${err.stack || err}`
+
+  return `${_name}${_statusCode}${_options}${_err}`
+}
+
+module.exports = (container, logger, handler, name, done) => {
+  const cb = typeof name === 'function'
+    ? name
+    : done
+
   try {
-    const res = await handler()
+    const resFn = handler(container)
 
-    if (!cb) return res
-    cb(null, res)
+    if (resFn instanceof Promise) {
+      if (!cb) {
+        return resFn
+          .catch((err) => {
+            logger.error(_prepareErrorData(err, name))
+
+            return Promise.reject(err)
+          })
+      }
+
+      resFn
+        .then((res) => cb(null, res))
+        .catch((err) => {
+          logger.error(_prepareErrorData(err, name))
+
+          cb(err)
+        })
+
+      return
+    }
+
+    if (!cb) return resFn
+    cb(null, resFn)
   } catch (err) {
-    const _name = name
-      ? `\n  - METHOD_NAME: ${name}`
-      : ''
-    const _statusCode = err.statusCode
-      ? `\n  - STATUS_CODE: ${err.statusCode}`
-      : ''
-    const _options = err.options
-      ? `\n  - OPTION: ${err.options}`
-      : ''
-    const _err = `\n  - ${err.stack || err}`
-
-    logger.error(`${_name}${_statusCode}${_options}${_err}`)
+    logger.error(_prepareErrorData(err, name))
 
     if (cb) cb(err)
     else throw err
