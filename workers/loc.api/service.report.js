6df4b151047e024104573dd24ad7ac6cf32fb84c
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-02-10 15:55:53 +0200

    Add logins method to main service

--- workers/loc.api/service.report.js
+++ workers/loc.api/service.report.js
@@ -5,6 +5,7 @@ const { Api } = require('bfx-wrk-api')
 const {
   checkParams,
   parseFields,
+  parseLoginsExtraDataFields,
   accountCache,
   getTimezoneConf,
   filterModels
@@ -407,6 +408,19 @@ class ReportService extends Api {
 
       return rest.accountSummary()
     }, 'getAccountSummary', cb)
+  }
+
+  getLogins (space, args, cb) {
+    return this._responder(() => {
+      return this._prepareApiResponse(
+        args,
+        'logins',
+        {
+          datePropName: 'time',
+          parseFieldsFn: parseLoginsExtraDataFields
+        }
+      )
+    }, 'getLogins', cb)
   }
 
   getMultipleCsv (space, args, cb) {
