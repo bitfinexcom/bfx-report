510c16536d4871174227fde3e3f4caa9a69ef0a3
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-06-29 12:45:43 +0300

    Add supported languages list

---
+++ workers/loc.api/queue/send-mail/languages.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  en: 'en',
+  'en-US': 'en',
+  ru: 'ru',
+  'zh-CN': 'zh-CN',
+  'zh-TW': 'zh-TW',
+  tr: 'tr',
+  'tr-TR': 'tr',
+  es: 'es-EM',
+  'es-EM': 'es-EM',
+  pt: 'pt-BR',
+  'pt-PT': 'pt-BR',
+  'pt-BR': 'pt-BR'
+}
