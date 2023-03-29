98794b74edf2a39a10f9872e0b844e1ddfea9362
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-03-29 07:19:44 +0300

    Add test case for getWeightedAveragesReport endpoint

--- test/1-api.spec.js
+++ test/1-api.spec.js
@@ -1719,4 +1719,48 @@ describe('API', () => {
       'note'
     ])
   })
+
+  it('it should be successfully performed by the getWeightedAveragesReport method', async function () {
+    this.timeout(120000)
+
+    const paramsArr = [
+      { end, start },
+      {
+        end,
+        start: end - (10 * 60 * 60 * 1000),
+        symbol: ['tBTCUSD']
+      }
+    ]
+
+    for (const params of paramsArr) {
+      const res = await agent
+        .post(`${basePath}/json-rpc`)
+        .type('json')
+        .send({
+          auth,
+          method: 'getWeightedAveragesReport',
+          params,
+          id: 5
+        })
+        .expect('Content-Type', /json/)
+        .expect(200)
+
+      assert.isObject(res.body)
+      assert.propertyVal(res.body, 'id', 5)
+      assert.isArray(res.body.result)
+
+      const resItem = res.body.result[0]
+
+      assert.isObject(resItem)
+      assert.containsAllKeys(resItem, [
+        'symbol',
+        'buyingWeightedPrice',
+        'buyingAmount',
+        'sellingWeightedPrice',
+        'sellingAmount',
+        'cumulativeWeightedPrice',
+        'cumulativeAmount'
+      ])
+    }
+  })
 })
