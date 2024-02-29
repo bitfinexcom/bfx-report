3eaaad15d4b5132c6c40fb0df8734a5751eef25e
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-02-29 09:04:55 +0400

    Set header/footer height into html-pdf lib

--- workers/loc.api/generate-report-file/pdf-writer/index.js
+++ workers/loc.api/generate-report-file/pdf-writer/index.js
@@ -110,6 +110,8 @@ class PdfWriter {
       template: 'No data',
       format: 'portrait',
       orientation: 'Letter',
+      headerHeight: '70mm',
+      footerHeight: '28mm',
       ...args
     }
 
