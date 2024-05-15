d46e649877854a257ef15f5a00ba58cab3426fd7
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-05-15 13:48:44 +0300

    Add interrupter param to BfxApiRouter

--- workers/loc.api/bfx.api.router/index.js
+++ workers/loc.api/bfx.api.router/index.js
@@ -4,7 +4,7 @@ const { decorateInjectable } = require('../di/utils')
 
 class BfxApiRouter {
   // Method does an idle job to be overridden in framework mode
-  route (methodName, method) {
+  route (methodName, method, interrupter) {
     return method()
   }
 }
