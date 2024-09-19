45f334526813dec6d71637fa19de2fce115bafaa
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-09-19 13:46:13 +0300

    Rework SendMail service to use i18next api

--- workers/loc.api/queue/send-mail/index.js
+++ workers/loc.api/queue/send-mail/index.js
@@ -1,23 +1,33 @@
 'use strict'
 
 const path = require('path')
-const fs = require('fs')
 const pug = require('pug')
-const yaml = require('js-yaml')
 
 const getTranslator = require('../../helpers/get-translator')
-const LANGUAGES = require('../../helpers/languages')
+const TRANSLATION_NAMESPACES = require(
+  '../../i18next/translation.namespaces'
+)
 
 const basePathToViews = path.join(__dirname, 'views')
-const pathToTrans = path.join(
-  __dirname,
-  'translations/email.yml'
-)
-const translations = yaml.load(
-  fs.readFileSync(pathToTrans, 'utf8')
-)
 
-module.exports = (grcBfxReq) => {
+const SENDGRID_WORKER_SUPPORTED_LNGS = {
+  en: 'en',
+  'en-US': 'en',
+  ru: 'ru',
+  'ru-RU': 'ru',
+  zh: 'zh-CN',
+  'zh-CN': 'zh-CN',
+  'zh-TW': 'zh-TW',
+  pt: 'pt-BR',
+  'pt-PT': 'pt-BR',
+  'pt-BR': 'pt-BR'
+}
+
+const _getSendgridLng = (lng) => {
+  return SENDGRID_WORKER_SUPPORTED_LNGS[lng] ?? lng
+}
+
+module.exports = (grcBfxReq, i18next) => {
   return async (
     configs,
     to,
@@ -30,12 +40,14 @@ module.exports = (grcBfxReq) => {
       const {
         presigned_url: url,
         language = 'en'
-      } = { ...data }
-      const normLang = LANGUAGES?.[language] ?? 'en'
-      const translate = getTranslator({
-        language: normLang,
-        translations
-      })
+      } = data ?? {}
+      const translate = getTranslator(
+        { i18next },
+        {
+          lng: language,
+          ns: TRANSLATION_NAMESPACES.EMAIL
+        }
+      )
       const subject = translate(
         configs.subject,
         'template.subject'
@@ -60,7 +72,7 @@ module.exports = (grcBfxReq) => {
         text,
         subject,
         button,
-        language: normLang
+        language: _getSendgridLng(language)
       }
 
       return grcBfxReq({
