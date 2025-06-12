2622e83ad8c9c802d8631c3b738fcfc3e42d76c9
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-12 13:50:58 +0300

    Add schema defs

---
+++ workers/loc.api/data-validator/schemas/defs.js
@@ -0,0 +1,20 @@
+'use strict'
+
+const SCHEMA_IDS = require('../schema.ids')
+
+module.exports = {
+  $id: SCHEMA_IDS.DEFS,
+  definitions: {
+    dateFormat: {
+      type: 'string',
+      enum: [
+        'DD-MM-YY',
+        'DD-MM-YYYY',
+        'MM-DD-YY',
+        'MM-DD-YYYY',
+        'YY-MM-DD',
+        'YYYY-MM-DD'
+      ]
+    }
+  }
+}
