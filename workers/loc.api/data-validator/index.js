1cf2b201cd16af169dfb180d47b96663716a88b7
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-13 11:50:36 +0300

    Implement validate fn using ajv

--- workers/loc.api/data-validator/index.js
+++ workers/loc.api/data-validator/index.js
@@ -2,6 +2,17 @@
 
 const Ajv = require('ajv')
 const addFormats = require('ajv-formats')
+const argv = require('yargs').argv
+
+const isDevEnv = (
+  argv.env === 'development' ||
+  process.env.NODE_ENV === 'development'
+)
+
+const {
+  ArgsParamsError,
+  ParamsValidSchemaFindingError
+} = require('../errors')
 
 const SCHEMA_DOMAIN = require('./schema.domain')
 const SCHEMA_NAMES = require('./schema.names')
@@ -9,9 +20,10 @@ const SCHEMA_IDS = require('./schema.ids')
 const schemas = require('./schemas')
 
 const ajv = new Ajv({
+  // Compile schema on initialization
   schemas: Object.values(schemas),
 
-  // strict mode
+  // Strict mode
   strict: true,
   strictRequired: true,
   allowMatchingProperties: true,
@@ -22,7 +34,7 @@ const ajv = new Ajv({
   allErrors: true,
   messages: true,
   formats: { reserved: true },
-  verbose: false
+  verbose: isDevEnv
 })
 addFormats(ajv)
 
@@ -40,8 +52,39 @@ const addSchemas = (schemas = []) => {
   }
 }
 
-// TODO:
-const validate = () => {}
+const validate = (args, schemaId) => {
+  const params = args?.params
+
+  if (
+    typeof params === 'undefined' ||
+    params === null
+  ) {
+    return true
+  }
+
+  const validate = ajv.getSchema(schemaId)
+
+  if (typeof validate !== 'function') {
+    throw new ParamsValidSchemaFindingError()
+  }
+
+  const res = validate(params)
+
+  // It covers a case when schema has `$async: true` keyword
+  if (res instanceof Promise) {
+    return res
+      .then(() => true)
+      .catch((err) => Promise.reject(
+        new ArgsParamsError({ data: err.errors })
+      ))
+  }
+
+  if (res) {
+    return true
+  }
+
+  throw new ArgsParamsError({ data: validate.errors })
+}
 
 module.exports = {
   SCHEMA_DOMAIN,
