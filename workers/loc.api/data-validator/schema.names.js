6c4c6c34f50b2b8bc8aa9507b40c6cae7e9a5172
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-17 14:08:27 +0300

    Add getStatusMessages req param schema

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -4,5 +4,6 @@ module.exports = {
   DEFS: 'defs',
   COMMON_REQ: 'commonReq',
   GET_WEIGHTED_AVERAGES_REPORT_REQ: 'getWeightedAveragesReportReq',
-  GET_PAY_INVOICE_LIST_REQ: 'getPayInvoiceListReq'
+  GET_PAY_INVOICE_LIST_REQ: 'getPayInvoiceListReq',
+  GET_STATUS_ME_REQ: 'getStatusMessagesReq'
 }
---
+++ workers/loc.api/data-validator/schemas/getStatusMessagesReq.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const SCHEMA_IDS = require('../schema.ids')
+
+module.exports = {
+  $id: SCHEMA_IDS.COMMON_REQ,
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    symbol: {
+      $ref: 'defs#/definitions/symbolWithMinItem'
+    },
+    type: {
+      type: 'string'
+    }
+  }
+}
