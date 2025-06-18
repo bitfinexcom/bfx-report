9be9fcd7a5bfc4310509c59b55aff4779d8c4e5b
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-18 08:12:51 +0300

    Add getPositionsAudit req param schema

---
+++ workers/loc.api/data-validator/schemas/getPositionsAuditReq.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const SCHEMA_IDS = require('../schema.ids')
+
+module.exports = {
+  $id: SCHEMA_IDS.GET_POSITIONS_AUDIT_REQ,
+  type: 'object',
+  additionalProperties: false,
+  required: ['id'],
+  properties: {
+    start: {
+      $ref: 'defs#/definitions/start'
+    },
+    end: {
+      $ref: 'defs#/definitions/end'
+    },
+    limit: {
+      $ref: 'defs#/definitions/limit'
+    },
+    symbol: {
+      $ref: 'defs#/definitions/symbolWithMinItem'
+    },
+    id: {
+      $ref: 'defs#/definitions/arrId'
+    },
+    notCheckNextPage: {
+      $ref: 'defs#/definitions/notCheckNextPage'
+    },
+    notThrowError: {
+      $ref: 'defs#/definitions/notThrowError'
+    },
+    isSyncRequest: {
+      $ref: 'defs#/definitions/isSyncRequest'
+    }
+  }
+}
