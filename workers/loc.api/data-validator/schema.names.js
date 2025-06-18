3b06a48df6daa47cc8fa2081dd9331a6b51381a5
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-18 09:34:53 +0300

    Add getCandles req param schema

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -8,5 +8,6 @@ module.exports = {
   GET_STATUS_MESSAGES_REQ: 'getStatusMessagesReq',
   GET_PUBLIC_TRADES_REQ: 'getPublicTradesReq',
   GET_POSITIONS_AUDIT_REQ: 'getPositionsAuditReq',
-  GET_ORDER_TRADES_REQ: 'getOrderTradesReq'
+  GET_ORDER_TRADES_REQ: 'getOrderTradesReq',
+  GET_CANDLES_REQ: 'getCandlesReq'
 }
