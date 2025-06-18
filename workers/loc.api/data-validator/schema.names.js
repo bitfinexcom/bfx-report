2060b8c85ec82d95ef34c9d82e8e6af973a21f5d
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-18 07:41:47 +0300

    Add getPublicTrades req param schema

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -5,5 +5,6 @@ module.exports = {
   COMMON_REQ: 'commonReq',
   GET_WEIGHTED_AVERAGES_REPORT_REQ: 'getWeightedAveragesReportReq',
   GET_PAY_INVOICE_LIST_REQ: 'getPayInvoiceListReq',
-  GET_STATUS_ME_REQ: 'getStatusMessagesReq'
+  GET_STATUS_MESSAGES_REQ: 'getStatusMessagesReq',
+  GET_PUBLIC_TRADES_REQ: 'getPublicTradesReq'
 }
---
+++ workers/loc.api/data-validator/schemas/getPublicTradesReq.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const SCHEMA_IDS = require('../schema.ids')
+
+module.exports = {
+  $id: SCHEMA_IDS.COMMON_REQ,
+  type: 'object',
+  additionalProperties: false,
+  properties: {
+    start: {
+      $ref: 'defs#/definitions/start'
+    },
+    end: {
+      $ref: 'defs#/definitions/end'
+    },
+    limit: {
+      $ref: 'defs#/definitions/limit'
+    },
+    symbol: {
+      $ref: 'defs#/definitions/symbolWithMaxItem'
+    },
+    notCheckNextPage: {
+      $ref: 'defs#/definitions/notCheckNextPage'
+    },
+    notThrowError: {
+      $ref: 'defs#/definitions/notThrowError'
+    },
+    isSyncRequest: {
+      $ref: 'defs#/definitions/isSyncRequest'
+    }
+  }
+}
