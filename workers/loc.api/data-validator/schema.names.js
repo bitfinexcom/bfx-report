e2c6e1bab0bea43cdaf7f06ed9f13b1edd402f2b
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-06-18 09:05:56 +0300

    Add getOrderTrades req param schema

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -7,5 +7,6 @@ module.exports = {
   GET_PAY_INVOICE_LIST_REQ: 'getPayInvoiceListReq',
   GET_STATUS_MESSAGES_REQ: 'getStatusMessagesReq',
   GET_PUBLIC_TRADES_REQ: 'getPublicTradesReq',
-  GET_POSITIONS_AUDIT_REQ: 'getPositionsAuditReq'
+  GET_POSITIONS_AUDIT_REQ: 'getPositionsAuditReq',
+  GET_ORDER_TRADES_REQ: 'getOrderTradesReq'
 }
