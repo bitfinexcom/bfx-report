cf17073a1bf692eed02a0723c695c53f55407fd8
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-10 12:23:22 +0300

    Add getWalletsFile req param schema name

--- workers/loc.api/data-validator/schema.names.js
+++ workers/loc.api/data-validator/schema.names.js
@@ -20,5 +20,6 @@ module.exports = {
   GET_MULTIPLE_FILE_REQ: 'getMultipleFileReq',
   GET_TICKERS_HISTORY_FILE_REQ: 'getTickersHistoryFileReq',
   GET_MOVEMENTS_FILE_REQ: 'getMovementsFileReq',
-  GET_LEDGERS_FILE_REQ: 'getLedgersFileReq'
+  GET_LEDGERS_FILE_REQ: 'getLedgersFileReq',
+  GET_WALLETS_FILE_REQ: 'getWalletsFileReq'
 }
