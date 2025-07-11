dc71a266b91383648394d5b10b524f078447c56a
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-11 11:19:18 +0300

    Add getPublicTradesFile req param schema

---
+++ workers/loc.api/data-validator/schemas/getPublicTradesFileReq.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const SCHEMA_IDS = require('../schema.ids')
+
+module.exports = {
+  $id: SCHEMA_IDS.GET_PUBLIC_TRADES_FILE_REQ,
+  type: 'object',
+  additionalProperties: false,
+  required: ['symbol'],
+  properties: {
+    start: {
+      $ref: 'defs#/definitions/start'
+    },
+    end: {
+      $ref: 'defs#/definitions/end'
+    },
+    limit: {
+      $ref: 'defs#/definitions/limit'
+    },
+    symbol: {
+      $ref: 'defs#/definitions/symbolWithMaxItem'
+    },
+    filter: {
+      $ref: 'defs#/definitions/filter'
+    },
+
+    email: {
+      $ref: 'defs#/definitions/email'
+    },
+    milliseconds: {
+      $ref: 'defs#/definitions/milliseconds'
+    },
+    dateFormat: {
+      $ref: 'defs#/definitions/dateFormat'
+    },
+    language: {
+      $ref: 'defs#/definitions/language'
+    },
+    timezone: {
+      $ref: 'defs#/definitions/timezone'
+    },
+    isPDFRequired: {
+      $ref: 'defs#/definitions/isPDFRequired'
+    },
+    isSignatureRequired: {
+      $ref: 'defs#/definitions/isSignatureRequired'
+    }
+  }
+}
