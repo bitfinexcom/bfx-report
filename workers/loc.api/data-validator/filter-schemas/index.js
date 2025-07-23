296a534114d621b309826c646e9aaf30592f5071
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-23 14:28:36 +0300

    Add entry point for filter schemas

---
+++ workers/loc.api/data-validator/filter-schemas/index.js
@@ -0,0 +1,6 @@
+'use strict'
+
+const FILTER_SCHEMA_NAMES = require('../filter.schema.names')
+const { requireSchemas } = require('../helpers')
+
+module.exports = requireSchemas(FILTER_SCHEMA_NAMES)
