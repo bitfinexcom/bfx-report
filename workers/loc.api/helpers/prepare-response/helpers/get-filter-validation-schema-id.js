8119ceb273246ebddaba3dc91b78521b5ea9461a
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-25 09:54:48 +0300

    Use filter api method names with schema id getter

--- workers/loc.api/helpers/prepare-response/helpers/get-filter-validation-schema-id.js
+++ workers/loc.api/helpers/prepare-response/helpers/get-filter-validation-schema-id.js
@@ -1,9 +1,11 @@
 'use strict'
 
+const FILTER_API_METHOD_NAMES = require('../../filter.api.method.names')
 const { FILTER_SCHEMA_IDS } = require('../../../data-validator')
 
 const PARAMS_SCHEMAS_MAP = {
-  positionsHistory: FILTER_SCHEMA_IDS.GET_POSITIONS_HISTORY_REQ_FILTER
+  [FILTER_API_METHOD_NAMES.POSITIONS_HISTORY]: FILTER_SCHEMA_IDS
+    .GET_POSITIONS_HISTORY_REQ_FILTER
 }
 
 module.exports = (
