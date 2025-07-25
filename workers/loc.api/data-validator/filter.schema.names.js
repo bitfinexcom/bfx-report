c91c8d9033c41e36ea12c9e84afa4a2f9e5fb687
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-25 11:38:03 +0300

    Add filter schema name for trades req filter

--- workers/loc.api/data-validator/filter.schema.names.js
+++ workers/loc.api/data-validator/filter.schema.names.js
@@ -1,5 +1,6 @@
 'use strict'
 
 module.exports = {
-  GET_POSITIONS_HISTORY_REQ_FILTER: 'getPositionsHistoryReqFilter'
+  GET_POSITIONS_HISTORY_REQ_FILTER: 'getPositionsHistoryReqFilter',
+  GET_TRADES_REQ_FILTER: 'getTradesReqFilter'
 }
