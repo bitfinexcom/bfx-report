f46e28907317d5c142bb2a1740b892c16f5b1b7f
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-25 09:34:38 +0300

    Fix filter schema import

--- workers/loc.api/data-validator/filter-schemas/index.js
+++ workers/loc.api/data-validator/filter-schemas/index.js
@@ -1,6 +1,11 @@
 'use strict'
 
+const path = require('node:path')
+
 const FILTER_SCHEMA_NAMES = require('../filter.schema.names')
 const { requireSchemas } = require('../helpers')
 
-module.exports = requireSchemas(FILTER_SCHEMA_NAMES)
+module.exports = requireSchemas(
+  FILTER_SCHEMA_NAMES,
+  path.join(__dirname, '../filter-schemas')
+)
