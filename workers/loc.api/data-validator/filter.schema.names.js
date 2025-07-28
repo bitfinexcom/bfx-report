219b233959cf94a05f58378829833529eebb8ae3
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-28 10:50:50 +0300

    Add filter schema name for funding trades req filter

--- workers/loc.api/data-validator/filter.schema.names.js
+++ workers/loc.api/data-validator/filter.schema.names.js
@@ -4,5 +4,6 @@ module.exports = {
   GET_POSITIONS_HISTORY_REQ_FILTER: 'getPositionsHistoryReqFilter',
   GET_TRADES_REQ_FILTER: 'getTradesReqFilter',
   GET_LEDGERS_REQ_FILTER: 'getLedgersReqFilter',
-  GET_TICKERS_HISTORY_REQ_FILTER: 'getTickersHistoryReqFilter'
+  GET_TICKERS_HISTORY_REQ_FILTER: 'getTickersHistoryReqFilter',
+  GET_FUNDING_TRADES_REQ_FILTER: 'getFundingTradesReqFilter'
 }
