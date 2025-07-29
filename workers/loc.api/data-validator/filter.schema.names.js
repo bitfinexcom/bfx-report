88a504cf752b4093a10241782ccb62fdbd1ae198
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-29 11:34:13 +0300

    Add filter schema name for public trades req filter

--- workers/loc.api/data-validator/filter.schema.names.js
+++ workers/loc.api/data-validator/filter.schema.names.js
@@ -5,5 +5,6 @@ module.exports = {
   GET_TRADES_REQ_FILTER: 'getTradesReqFilter',
   GET_LEDGERS_REQ_FILTER: 'getLedgersReqFilter',
   GET_TICKERS_HISTORY_REQ_FILTER: 'getTickersHistoryReqFilter',
-  GET_FUNDING_TRADES_REQ_FILTER: 'getFundingTradesReqFilter'
+  GET_FUNDING_TRADES_REQ_FILTER: 'getFundingTradesReqFilter',
+  GET_PUBLIC_TRADES_REQ_FILTER: 'getPublicTradesReqFilter'
 }
