22dfd173d7381c5d42e7746c01adcc90dff033bc
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-29 11:48:05 +0300

    Add filter schema name for movements req filter

--- workers/loc.api/data-validator/filter.schema.names.js
+++ workers/loc.api/data-validator/filter.schema.names.js
@@ -7,5 +7,6 @@ module.exports = {
   GET_TICKERS_HISTORY_REQ_FILTER: 'getTickersHistoryReqFilter',
   GET_FUNDING_TRADES_REQ_FILTER: 'getFundingTradesReqFilter',
   GET_PUBLIC_TRADES_REQ_FILTER: 'getPublicTradesReqFilter',
-  GET_ORDERS_REQ_FILTER: 'getOrdersReqFilter'
+  GET_ORDERS_REQ_FILTER: 'getOrdersReqFilter',
+  GET_MOVEMENTS_REQ_FILTER: 'getMovementsReqFilter'
 }
