86ab059e724a5f7784660ab2a86382161dceb47f
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-07-30 11:21:02 +0300

    Add filter schema for status messages req filter

---
+++ workers/loc.api/data-validator/filter-schemas/getStatusMessagesReqFilter.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const FILTER_SCHEMA_IDS = require('../filter.schema.ids')
+const { getFilterSchema } = require('./helpers')
+
+module.exports = getFilterSchema(
+  FILTER_SCHEMA_IDS.GET_STATUS_MESSAGES_REQ_FILTER
+)
