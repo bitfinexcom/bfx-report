c185d279dd450d30a9561b315d75689957ef3165
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-09-16 16:48:47 +0300

    Add ability to handle interrupter in bfx api router

--- workers/loc.api/bfx.api.router/index.js
+++ workers/loc.api/bfx.api.router/index.js
@@ -1,11 +1,49 @@
 'use strict'
 
+const Interrupter = require('../interrupter')
 const { decorateInjectable } = require('../di/utils')
 
 class BfxApiRouter {
   // Method does an idle job to be overridden in framework mode
   route (methodName, method, interrupter) {
-    return method()
+    return this.execMethod(method, interrupter)
+  }
+
+  execMethod (method, interrupter) {
+    if (!(interrupter instanceof Interrupter)) {
+      return method()
+    }
+    if (interrupter.hasInterrupted()) {
+      return
+    }
+
+    const res = method()
+
+    if (!(res instanceof Promise)) {
+      return res
+    }
+
+    let onceInterruptHandler = null
+    const intPromise = new Promise((resolve) => {
+      onceInterruptHandler = () => {
+        onceInterruptHandler = null
+        resolve()
+      }
+
+      interrupter.onceInterrupt(onceInterruptHandler)
+    })
+
+    return Promise.race([
+      res,
+      intPromise
+    ]).finally(() => {
+      if (typeof onceInterruptHandler !== 'function') {
+        return
+      }
+
+      interrupter.offInterrupt(onceInterruptHandler)
+      onceInterruptHandler()
+    })
   }
 }
 
